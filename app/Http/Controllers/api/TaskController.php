7baa1428c633ba0df7c14c7d2f23c5fff485d926
unknown <esdesuagk@gmail.com>
2023-09-19 17:13:33 +0300

    controller, request, resource

---
+++ app/Http/Controllers/api/TaskController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use app\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show($id)
+    {
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
+    }
+}
