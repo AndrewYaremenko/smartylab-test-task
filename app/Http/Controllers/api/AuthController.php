174be55bacf0a3f0b9774f80702c387b20e0bdb3
unknown <esdesuagk@gmail.com>
2023-09-19 21:45:02 +0300

    laravel passport

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        $user->save();
+
+        $token = $user->createToken('MyApp')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($credentials)) {
+            $token = auth()->user()->createToken('MyApp')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthenticated'], 401);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\TaskController;
+use App\Http\Controllers\api\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
 
-Route::prefix('tasks')->group(function () {
-    Route::get('/', [TaskController::class, "index"]);
-    Route::get('{id}', [TaskController::class, "show"]);
-    Route::post('/', [TaskController::class, "store"]);
-    Route::put('{id}', [TaskController::class, "update"]);
-    Route::delete('{id}', [TaskController::class, "destroy"]);
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::prefix('tasks')->group(function () {
+        Route::get('/', [TaskController::class, "index"]);
+        Route::get('{id}', [TaskController::class, "show"]);
+        Route::post('/', [TaskController::class, "store"]);
+        Route::put('{id}', [TaskController::class, "update"]);
+        Route::delete('{id}', [TaskController::class, "destroy"]);
+    });
 });
 
 //Есть вариант использовать Route::resource('tasks', 'TaskController'), но в данном случае решил прописать все вручную
